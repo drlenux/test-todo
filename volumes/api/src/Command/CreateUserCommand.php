73f3b2a4e01eb2e98a5256128f193e8d9ca2da4d
eselbelokudrenko <oleksii.bielokudrenko@gmail.com>
2024-05-01 15:21:48 +0300

    add jwt auth

---
+++ volumes/api/src/Command/CreateUserCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:user:create',
+    description: 'create user',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'email')
+            ->addArgument('password', InputArgument::REQUIRED, 'password');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        if ($this->userRepository->findOneBy(['email' => $email])) {
+            $io->error('User exists');
+            return Command::FAILURE;
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $hash = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hash);
+        $this->userRepository->save($user);
+
+        $io->success('User created');
+
+        return Command::SUCCESS;
+    }
+}
